04d78c629aed8cae6c27b5e15f2f23ff72d6ef30
Prijith T <prijitht4@gmail.com>
2023-11-28 15:35:27 +0530

    Header, section and footer added

---
+++ app.js
@@ -0,0 +1,55 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const { engine } = require('express-handlebars');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// Set up Handlebars engine using destructured engine() function
+app.engine('.hbs', engine({
+  defaultLayout: 'layout',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layout'), // Path to your layout folder
+  partialsDir: [
+    path.join(__dirname, 'views/partials') // Path to your partials folder
+    
+  ]
+}));
+app.set('view engine', '.hbs');
+
+// Setting views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Middleware setup
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Routes handling
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// Error handler
+app.use(function(err, req, res, next) {
+  // Set locals, providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // Render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+var cards=[
+  {
+  image:'https://i.redd.it/2o648aerezg51.png',
+  titles:'Static Web Site',
+  texts:'This service provides a static web application using HTML, CSS,JS'
+},
+{
+  image:'https://www.tesronwebtech.com/images/img/static.jpg',
+  titles:'Responsive Static Web Site',
+  texts:'This service provides a responsive web application using HTML, CSS,JS,Bootstrap'
+},
+{
+  image:'https://th.bing.com/th/id/R.a0554f2778fde4673d547828ea5e7642?rik=HH0qiWGFMyosHA&riu=http%3a%2f%2fwww.eworker.co%2fcourses%2fwp-content%2fuploads%2f2017%2f09%2fcourse-nodejs-mongo-db2.png&ehk=JTWpYW7b1p1Kb0V0%2fQE824pn9rwXhdjiBQPyhbMOYsg%3d&risl=&pid=ImgRaw&r=0',
+  titles:'Full stack web application',
+  texts:'This service provides a full stack application using Nodejs Express and MongoDB database'
+}
+]
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express',cards });
+});
+
+module.exports = router;
